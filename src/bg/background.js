f48b8fa9dc4d3ba4d664a7651f47c3522d5fd5c1
liakwee <liakwee.lee@xm-asia.com>
2014-07-18 18:00:38 +0800

    initial Commit

--- src/bg/background.js
+++ src/bg/background.js
@@ -6,8 +6,14 @@
 
 
 //example of using a message handler from the inject scripts
+/*
 chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+    function (request, sender, sendResponse) {
+        chrome.pageAction.show(sender.tab.id);
+        sendResponse();
+    });*/
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.tabs.executeScript(tab.id, {
+        file: 'src/inject/inject.js'
+    });
+});
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,48 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+function searching() {
+    console.log("startSearching");
+    var h1 = $('body').find("h1");
+    var h2 = $('body').find("h2");
+    var h3 = $('body').find("h3");
+    var h4 = $('body').find("h4");
+    var h5 = $('body').find("h5");
+    var h6 = $('body').find("h6");
 
-	}
-	}, 10);
-});
+    for(var i=1; i<=6; i++){
+        $('body').find('h'+i).addClass('ch-h'+i+'-tags');
+        $('body').find('h'+i).attr('name', 'h'+i);
+        $('body').find('h'+i).attr('title', 'h'+i);
+    }
+
+
+
+    var name = "#ch-floatMenu";
+    var floatHTML = "<div id='ch-floatMenu'><ul><li class='ch-h1-tags'>H1 Tags</li><li class='ch-h2-tags'>H2 Tags</li>"+
+        "<li class='ch-h3-tags'>H3 Tags</li><li class='ch-h4-tags'>H4 Tags</li><li class='ch-h5-tags'>H5 Tags</li>"+
+        "<li class='ch-h6-tags'>H6 Tags</li></ul></div>";
+
+
+
+    $('body').append(floatHTML);
+
+    $(name).fadeIn(1000);
+
+
+    var menuYloc = 0;
+    $(window).scroll(function () {
+        var offset = menuYloc+$(document).scrollTop()+"px";
+        $(name).animate({top:offset},{duration:500,queue:false});
+
+    });
+
+}
+
+
+function header(){
+    $(":header").css({ background:'#CCC', color:'blue' });
+}
+
+
+
+
+searching();
